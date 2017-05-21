85b63e30c806edb692d4cc54e697ae2a1fd65bc8
Ham2604 <mooham2003@hotmail.com>
2017-05-21 17:48:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'Uzp4grHqTKjAug5QCrh1Y5y5+VQbqJvq/tfaVkaBISEww3I55scjWCGvRmrq/CWxzF6IWo7oGKLVgpVEgflaWJemtJPLra3YQOJ3s0RbpyK5gXvof/UtOA/7X2kmeiM6ZVmIK/5DN392lJrGSIMecgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
